86ebaa776d5b963bf5c3b987c43fdcb5f4f055c9
TheusH7X5 <matheush7x5@gmail.com>
2023-03-15 21:00:41 -0300

    subindo fotos

--- src/api/apiPortfolio.jsx
+++ src/api/apiPortfolio.jsx
@@ -1,37 +1,37 @@
 export const apiPortfolio = [
   {
     id: 0,
-    src: "src/assets/dashboard.gif",
+    src: "https://i.ibb.co/wNxJdsz/dashboard.gif",
     alt: "dashboard",
     link: "https://github.com/TheusH7X5/DashGo",
   },
   {
     id: 1,
-    src: "src/assets/home_ignews.gif",
+    src: "https://i.ibb.co/h8WVGJT/home-ignews.gif",
     alt: "home ignews",
     link: "https://github.com/TheusH7X5/ignewsH7X5",
   },
   {
     id: 2,
-    src: "src/assets/bytebank-angular.gif",
+    src: "https://i.ibb.co/TbDZXp7/bytebank-angular.gif",
     alt: "Transferencia Angular",
     link: "https://github.com/TheusH7X5/TransferenciasAngular",
   },
   {
     id: 3,
-    src: "src/assets/labcoodesh.jpeg",
+    src: "https://i.ibb.co/HtDpZ1s/labcoodesh.jpg",
     alt: "case labcoodesh",
     link: "https://github.com/TheusH7X5/case-labcoodesh",
   },
   {
     id: 4,
-    src: "src/assets/fundamentos-reactjs-ts.png",
+    src: "https://i.ibb.co/568t89v/fundamentos-reactjs.png",
     alt: "fundamentos react typescript",
     link: "https://github.com/TheusH7X5/01-fundamentos-reactjs-ts",
   },
   {
     id: 5,
-    src: "src/assets/dt-money.png",
+    src: "https://i.ibb.co/tqT9BbQ/dt-money.png",
     alt: "dtmoney",
     link: "https://github.com/TheusH7X5/dtmoney",
   },
