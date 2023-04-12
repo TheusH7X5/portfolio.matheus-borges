53fe70a111885a2f33dbaa88bdf334acd7efbddb
TheusH7X5 <matheush7x5@gmail.com>
2023-04-12 19:45:08 -0300

    ajustes

--- src/api/urls.jsx
+++ src/api/urls.jsx
@@ -1,8 +1,9 @@
 export const urlDownload =
-  "https://drive.google.com/uc?export=download&id=1RqFnb82-EDWIsLS8oLACLzmSKWuAWd0y";
+  "https://drive.google.com/uc?export=download&id=1aCngPd5wWzHc3BFmjAP2teR4dQU0L2f8";
 
-export const imgProfileGitHub = "https://github.com/theusH7X5.png"
+export const imgProfileGitHub = "https://github.com/theusH7X5.png";
 
-export const nameProfileGitHub = "Matheus Borges"
+export const nameProfileGitHub = "Matheus Borges";
 
-export const downloadCurriculumLink = "https://drive.google.com/uc?export=download&id=1VJ5aQ_qp6J3cKwA3yeKejeOuTh8sjWE5"
+export const downloadCurriculumLink =
+  "https://drive.google.com/uc?export=download&id=1aCngPd5wWzHc3BFmjAP2teR4dQU0L2f8";
